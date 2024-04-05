62efcee8e16ef6c095089c9412252a304de0375e
Ranit Manik <138437760+RanitManik@users.noreply.github.com>
2024-04-05 17:23:14 +0530

    added code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,23 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#body-wrapper'),
     smooth: true
 });
+
+const navElement = document.getElementById('navElement');
+const mediaQuery = window.matchMedia('(max-width: 40rem)');
+
+const handleMediaQueryChange = (mediaQuery) => {
+    if (mediaQuery.matches) {
+        navElement.innerHTML = `<ul><li><a href="#">Menu</a></li></ul>`;
+    } else {
+        navElement.innerHTML = `<ul>
+                <li><a href="#">Work</a></li>
+                <li><a href="#">Studio</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>`;
+    }
+};
+
+handleMediaQueryChange(mediaQuery);
+
+mediaQuery.addEventListener('change', handleMediaQueryChange);
+
