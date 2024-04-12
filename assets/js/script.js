96ea5dd55f1518b9e54bacb8170522730784dca4
Ranit Manik <138437760+RanitManik@users.noreply.github.com>
2024-04-12 11:34:30 +0530

    added code

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,39 +4,32 @@ const scroll = new LocomotiveScroll({
 });
 
 const navElement = document.getElementById('navElement');
-const mediaQuery = window.matchMedia('(max-width: 40rem)');
+const mediaQueryMobile = window.matchMedia('(max-width: 40rem)');
+const mediaQueryTablet = window.matchMedia('(max-width: 75rem)');
+const mediaQueryLaptop = window.matchMedia('(min-width: 75rem)');
 
-const handleMediaQueryChange = (mediaQuery) => {
-    if (mediaQuery.matches) {
-        navElement.innerHTML = `<ul><li><a href="#"><div class="menu-icon"><div></div><div></div></div>Menu</a></li></ul>`;
-    } else {
-        navElement.innerHTML = `<ul>
+if (mediaQueryMobile.matches) {
+    navElement.innerHTML = `<ul><li><a href="#"><div class="menu-icon"><div></div><div></div></div>Menu</a></li></ul>`;
+} else {
+    navElement.innerHTML = `<ul>
                 <li><a href="#">Work</a></li>
                 <li><a href="#">Studio</a></li>
                 <li><a href="#">Contact</a></li>
             </ul>`;
-    }
-};
-
-handleMediaQueryChange(mediaQuery);
-
-mediaQuery.addEventListener('change', handleMediaQueryChange);
+}
 
 let fixedItemContainer = document.querySelector(".fixed-image-container");
 let projectItems = document.querySelectorAll(".featured-projects ul li");
 
-projectItems.forEach(fixedItem => {
-    fixedItem.addEventListener("mouseenter", () => {
-        fixedItemContainer.style.display = "block";
-        let dataImage = fixedItem.getAttribute("data-image");
-        let dataType = fixedItem.getAttribute("data-type");
-        if (dataType === "image") {
-            fixedItemContainer.innerHTML = `<img src="${dataImage}" alt="">`;
-        } else if (dataType === "video") {
-            fixedItemContainer.innerHTML = `<video autoplay muted loop                   src="${dataImage}"></video>`;
-        }
+if (mediaQueryLaptop.matches) {
+    projectItems.forEach(fixedItem => {
+        fixedItem.addEventListener("mouseenter", () => {
+            fixedItemContainer.style.display = "block";
+            let dataInfo = fixedItem.querySelector(".project-asset-container");
+            fixedItemContainer.innerHTML = dataInfo.innerHTML;
+        });
+        fixedItem.addEventListener("mouseleave", () => {
+            fixedItemContainer.style.display = "none";
+        });
     });
-    fixedItem.addEventListener("mouseleave", () => {
-        fixedItemContainer.style.display = "none";
-    });
-});
+}
